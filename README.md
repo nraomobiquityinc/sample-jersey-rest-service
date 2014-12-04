5c580289c0d6f042432d88979f4f35febcf7ea06
Neeraj Rao <nrao@mobiquityinc.com>
2014-12-04 17:27:04 -0500

    Adding README.

---
+++ README.md
@@ -0,0 +1,10 @@
+Sample Jersey REST Service
+---
+
+Import as Maven Project, build and deploy on server.
+
+To see text response:
+curl -H "Accept: text/plain" http://localhost:8080/sample-jersey-rest-service/rest/testresource
+
+To see HTML response:
+curl -H "Accept: text/html" http://localhost:8080/sample-jersey-rest-service/rest/testresource
--- src/main/java/com/mobiquityinc/sample/jersey/MyResource.java
+++ src/main/java/com/mobiquityinc/sample/jersey/MyResource.java
@@ -11,19 +11,19 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("testresource")
 public class MyResource {
-	
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String returnPlainText() {
-		return "Success";
-	}
+    
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String returnPlainText() {
+        return "Success";
+    }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
     public String returnHTML() {
         return "<html>" +
-        	   "<title>Test REST response</title>" + 
-        	   "<body><h1>Success!</h1></body>" +
-        	   "</html>";
+               "<title>Test REST response</title>" + 
+               "<body><h1>Success!</h1></body>" +
+               "</html>";
     }
 }
